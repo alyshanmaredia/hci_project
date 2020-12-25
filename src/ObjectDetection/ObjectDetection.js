68cb494f33c4e85186632f66c22427d685fa0ba5
alyshanmaredia <alyshanmaredia@gmail.com>
2020-12-25 16:13:26 +0500

    update object detection code

--- src/ObjectDetection/ObjectDetection.js
+++ src/ObjectDetection/ObjectDetection.js
@@ -9,39 +9,36 @@ function ObjectDetection() {
   const webcamRef = useRef(null);
   const canvasRef = useRef(null);
 
-  // Main function
   const runCoco = async () => {
     const net = await cocossd.load();
-    //  Loop and detect hands
+    
     setInterval(() => {
       detect(net);
     }, 10);
   };
 
   const detect = async (net) => {
-    // Check data is available
+  
     if (
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null &&
       webcamRef.current.video.readyState === 4
     ) {
-      // Get Video Properties
+
       const video = webcamRef.current.video;
       const videoWidth = webcamRef.current.video.videoWidth;
       const videoHeight = webcamRef.current.video.videoHeight;
 
-      // Set video width
+     
       webcamRef.current.video.width = videoWidth;
       webcamRef.current.video.height = videoHeight;
 
-      // Set canvas height and width
       canvasRef.current.width = videoWidth;
       canvasRef.current.height = videoHeight;
 
       const obj = await net.detect(video);
       console.log(obj);
 
-      // Draw mesh
       const ctx = canvasRef.current.getContext("2d");
 
       drawRect(obj, ctx);  
